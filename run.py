5a6e8542b005d9fe21767e5b0d3db93d70136bf9
S.P. <s.protsenk@gmail.com>
2024-03-11 13:47:04 +1100

    task 8 added

--- run.py
+++ run.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 from pathlib import PurePath, Path
 
 app = Flask(__name__)
 
+app.secret_key = b'123456789'
+
 
 @app.route('/')
 def index():
     context = [
-        ['task 7', 'power'],
-        ['task 8', 'flash'],
-        ['task 9', 'cookie']
+        ['task 7', 'power_get'],
+        ['task 8', 'flash_get'],
+        ['task 9', 'cookie_get']
     ]
     return render_template('about.html', context=context)
 
 
 # task 7
 @app.get('/power')
-def power():
+def power_get():
     context = {
         "title": "Task 7 - Power",
         "task": '''Создать страницу, на которой будет форма для ввода числа
@@ -26,6 +28,7 @@ def power():
 выведено введенное число и его квадрат.'''
     }
     return render_template('power_t07.html', **context)
+
 
 @app.post('/power')
 def power_post():
@@ -37,15 +40,32 @@ def power_post():
     return f'Your number was {number}.<br>Square of this number: {number ** 2}', 200
 
 
-
+# task 8
 @app.get('/flash')
-def flash():
-    pass
+def flash_get():
+    context = {
+        "title": "Task 8",
+        "task": '''Создать страницу, на которой будет форма для ввода имени
+и кнопка "Отправить". При нажатии на кнопку будет произведено
+перенаправление на страницу с flash сообщением, где будет
+выведено "Привет, {имя}!".'''
+    }
+    return render_template('flash_form.html', **context)
+
+
+@app.post('/flash')
+def flash_post():
+    name = request.form.get('name')
+    if not name:
+        flash(message='Please enter your name', category='danger')
+        return redirect(url_for('flash_get'))
+    flash(message='Hello, {}!'.format(name), category='success')
+    return redirect(url_for('flash_get'))
 
 
 # task9
 @app.get('/cookie')
-def cookie():
+def cookie_get():
     pass
 
 
